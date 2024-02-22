0240b808cf2a1d4fff0b0be621b80c3a9f0548bf
darespinoza <despinoza@uazuay.edu.ec>
2024-02-22 15:08:04 -0500

    First commit

---
+++ example_page.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Example page for IERSE header and footer</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+        <!-- Bootstrap 5.3.2   -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Bootstrap Icons   -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <!-- jQuery 3.5.1   -->
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+
+        <link href="style/style_bar_bs5_v2.css" rel="stylesheet">
+    </head>
+    <body>
+        <!-- Header IERSE 2023 -->
+        <?php include("header_ierse_v2.html");?>
+
+        <div class="container mt-5">
+            <h3>Navbar Forms</h3>
+            <p>You can also include forms inside the navigation bar.</p>
+            
+            <p>
+                Proident in velit mollit deserunt enim aliquip commodo sit incididunt laboris elit ullamco ullamco aliquip. Nostrud esse deserunt ex est dolor commodo. Laborum ea aliqua ex aliquip ea dolore do sit minim.
+            </p>
+
+            <p>
+                Nisi voluptate duis sit incididunt. Veniam fugiat aliqua dolor non consectetur occaecat ad veniam ut in aliqua sint commodo incididunt. Laboris magna aliquip fugiat mollit.
+
+                Reprehenderit anim tempor veniam Lorem id esse ullamco. Est do culpa id anim consequat nisi. Tempor reprehenderit enim non do quis laboris dolore quis consectetur consectetur incididunt aliqua minim excepteur. Reprehenderit sint eu laboris incididunt magna consectetur quis exercitation sit minim aliqua nulla aute cupidatat. Fugiat excepteur Lorem sint non esse elit fugiat.
+
+                Voluptate labore sint tempor minim cillum pariatur qui. Tempor ad velit laborum dolore esse. Fugiat id laborum in cupidatat magna adipisicing occaecat voluptate. Reprehenderit aliqua fugiat deserunt ipsum ullamco. Mollit ad pariatur enim ex ullamco id. Ea eiusmod nisi velit cupidatat ex consectetur reprehenderit officia commodo tempor amet.
+            </p>
+
+            <p>
+                Proident labore ad culpa id id. Elit nisi minim irure duis cupidatat est veniam esse non. Mollit dolor proident velit anim anim consectetur. Sit do officia excepteur ullamco fugiat. Ea do dolore incididunt reprehenderit tempor irure occaecat proident id incididunt consequat occaecat eiusmod. Aliqua nisi deserunt anim exercitation duis ad et deserunt ut veniam.
+
+                Anim exercitation consequat fugiat eiusmod irure ex consequat. Pariatur ut mollit elit laborum veniam do voluptate commodo quis consequat non. Consequat ullamco id magna tempor laborum ut Lorem reprehenderit magna excepteur. Voluptate ea deserunt adipisicing incididunt sunt ut ut aliquip anim cillum dolore veniam est. Sunt sunt minim et laborum occaecat ullamco.
+
+                Eiusmod magna ullamco anim fugiat laboris sit in nisi labore tempor fugiat. Id laboris pariatur mollit commodo aute ullamco aute deserunt occaecat commodo. Qui ipsum nulla aliqua exercitation qui laboris irure excepteur aliqua sit nulla sunt sit. Veniam deserunt esse adipisicing mollit dolor. Ex eu deserunt laboris velit consectetur reprehenderit eiusmod excepteur occaecat. Officia eiusmod proident commodo velit. Ut minim aliqua labore amet mollit nisi fugiat sunt ea anim sint minim mollit.
+
+                Nulla duis eiusmod excepteur laboris. Ipsum labore pariatur aute sit do aliqua proident duis. Et Lorem aliquip ea exercitation exercitation eiusmod non dolor aliquip cillum quis veniam aliqua cillum. Nostrud ullamco elit est adipisicing deserunt dolore duis anim ullamco magna pariatur. Id ea anim elit proident amet aliqua id consequat pariatur ad dolor exercitation aute culpa. Cupidatat aliqua laboris aliquip aliquip.
+
+                Laborum nulla minim nisi consequat sunt ad aliqua id pariatur aliquip. Excepteur ad anim adipisicing esse exercitation aliquip excepteur veniam mollit duis aliqua. Velit tempor ad fugiat fugiat proident sunt eiusmod reprehenderit anim eu labore. Elit eiusmod esse laborum esse ad adipisicing sunt non tempor voluptate. Incididunt nulla laboris eiusmod sint ea labore incididunt incididunt deserunt.
+            </p>
+        </div>
+
+        <!-- Footer IERSE 2023 -->
+        <?php include("footer_ierse_v2.html");?>
+
+        <!-- Funciones y estilos para barras   -->
+        <script src="lib/function_bars_bs5_v2.js"></script>
+
+    </body>
+</html>
